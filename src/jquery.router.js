45fbea3ae7d594cab96a2212f4e29f2e52713dc0
wu yuntao <wyt.brandon@gmail.com>
2010-09-27 10:01:07 +0800

    Create tests and demos.

---
+++ src/jquery.router.js
@@ -0,0 +1,13 @@
+/**
+ * jQuery Router Plugin
+ *
+ * Copyright (c) 2010 Wu Yuntao
+ *
+ * Licensed under the MIT License:
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+
+(function($) {
+
+})(jQuery);
---
+++ tests/router.js
@@ -0,0 +1,9 @@
+(function($) {
+
+module("Router TestCases");
+
+test("Should be true", function() {
+    equals(true, true);
+});
+
+})(jQuery);
