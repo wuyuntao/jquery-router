227e68ab432310f03846b3c589eef04e81fa3777
wuyuntao <wyt.brandon@gmail.com>
2010-11-15 12:58:21 +0800

    Refactor route plugin to make it work with jquery mobile

--- src/jquery.router.js
+++ src/jquery.router.js
@@ -8,349 +8,144 @@
  *
  */
 
-// (function($) {
+(function($) {
 
-/**
- * Represents a single route and can parse the dynamic route syntax
- */
-var Route = function(route, target, name, static, router) {
-    this.init(route, target, name, static, router);
-};
+    var routes = [],
+        statics = {},
+        dynamics = [],
+        compiled = false,
+        // javaScript does not support negative lookbehind assertion
+        // syntax: /(?<!\\):([a-zA-Z_]+)?(?:#(.*?)#)?/i,
+        syntax = /:([a-zA-Z_]+)?(?:#(.*?)#)?/i,
+        base = '[^/]+';
 
-$.extend(Route.prototype, {
-    // JavaScript does not support negative lookbehind assertion
-    // syntax: /(?<!\\):([a-zA-Z_]+)?(?:#(.*?)#)?/i,
-    syntax: /:([a-zA-Z_]+)?(?:#(.*?)#)?/i,
+    // expose public methods
+    $.extend($, {
+        route: route,
+        routeMatches: routeMatches,
+        unparam: unparam
+    });
 
-    base: '[^/]+',
+    // a jquery plugin to create a route
+    $.fn.route = function(route, callback, sync) {
+        if (!this.length) return this;
 
-    /**
-     * Create a Route.
-     * The route string may contain `:key`, `:key#regexp#` or `:#regexp#` tokens
-     * for each dynamic part of the route. These can be escaped with a backslash
-     * in front of the `:` and are completely ignored if static is true.
-     * A name may be used to refer to this route later (depends on Router)
-     */
-    init: function(route, target, name, static, router) {
-        this.route = route.replace('\\:', ':');
-        if ($.isArray(target)) {
-            this.targets = target;
-        } else if (!target) {
-            this.targets = [];
-        } else {
-            this.targets = [target];
-        }
-
-        this.name = name;
-        this.realroute = static ? route.replace(':', '\\:') : route;
-        this.tokens = this.realroute.split(this.syntax);
-        this.router = router;
-    },
-
-    /**
-     * Append a (named) target
-     */
-    get: function(target) {
-        this.targets.push(target);
+        $.route(route, this, callback, sync);
         return this;
-    },
+    };
 
-    /**
-     * Return the router
-     */
-    end: function() {
-        return this.router;
-    },
+    // create a route.
+    // the route string may contain `:key`, `:key#regexp#` or `:#regexp#` tokens
+    // for each dynamic part of the route. These can be escaped with a backslash
+    // in front of the `:` and are completely ignored if static is true.
+    function route(route, template, callback, async) {
+        var route = {
+            route: route.replace('\\:', ':'),
+            realroute: route,
+            template: template,
+            tokens: route.split(syntax),
+            callback: callback,
+            async: async
+        };
+        if (route.template && !routeExists(route)) {
+            routes.push(route);
+            compiled = false;
+        }
+    }
 
-    /**
-     * Return a regexp pattern with groups
-     */
-    group: function() {
+    // check if route is already defined
+    function routeExists(route) {
+        for (var i = 0, len = routes.length; i < len; ++i)
+            if (route.realroute == routes[i].realroute)
+                return true;
+    }
+
+    // return a regexp pattern with groups
+    function group(route) {
         out = '';
-        $.each(this.tokens, $.proxy(function(i, part) {
+        $.each(route.tokens, function(i, part) {
             if (i % 3 == 0) {
                 out += part.replace('\:', ':').replace('(', '\\(').replace(')', '\\)');
             } else if (i % 3 == 1) {
-                out += '(';   // Javascript does not support named groups
+                out += '(';   // javascript does not support named groups
             } else {
-                out += (part || this.base) + ')';
-            }
-        }, this));
-        return out;
-    },
-
-    /**
-     * Return a format string with named fields.
-     */
-    format: function() {
-        var out = '', c = 0;
-        $.each(this.tokens, function(i, part) {
-            if (i % 3 == 0) {
-                out += part.replace('\\:', ':').replace('%', '%%');
-            } else if (i % 3 == 1) {
-                if (!part) part = 'anon' + (c++);
-                out += '#{' + part + '}';
+                out += (part || base) + ')';
             }
         });
         return out;
-    },
-
-    static: function() {
-        return this.tokens.length == 1;
-    },
-
-    equals: function(route) {
-        return this.realroute === route.realroute;
     }
-});
 
-/**
- * A route associates a string (e.g. URL) with an object (e.g. function)
- * Some dynamic routes may extract parts of the string and provide them as
- * a dictionary. This router matches a string against multiple routes and
- * returns the associated object along with the extracted data.
- */
-var Router = function() {
-    this.init();
-}
+    // check if route is static
+    function isStatic(route) {
+        route.tokens.length == 1;
+    }
 
-$.extend(Router.prototype, {
-    init: function() {
-        this.routes  = [];  // List of all installed routes
-        this.named   = {};  // Cache for named routes and their format strings
-        this.static  = {};  // Cache for static routes
-        this.dynamic = [];  // Search structure for dynamic routes
-        this.compiled = false;
-    },
-
-    /**
-     * Add a route->target pair or a :class:`Route` object to the Router.
-     * Return the Route object. See :class:`Route` for details.
-     */
-    add: function(route, target, options) {
-        options = options || {};
-        if (typeof route == 'string') {
-            route = new Route(route, target, options.name, options.static, this);
+    // match a url and return a route object
+    function routeMatches(url) {
+        var request = parseUrl(url);
+        if (request.path in statics) {
+            return {route: statics[request.path], args: [request]};
         }
-        var known = this.get(route);
-        if (known) return known;
-        this.routes.push(route);
-        this.reset();
-        if (options.compile) this.compile();
-        return route;
-    },
-
-    /**
-     * Set compiled flag to false and clean route caches
-     */
-    reset: function() {
-        this.compiled = false;
-        this.named = {};
-        this.static = {};
-        this.dynamic = [];
-    },
-
-    /**
-     * Get a route from the router by specifying either the same
-     * parameters as in :meth:`add` or comparing to an instance of
-     * :class:`Route`. Note that not all parameters are considered by the
-     * compare function.
-     */
-    get: function(route, target, options) {
-        options = options || {};
-        if (typeof route == 'string') {
-            route = new Route(route, target, options.name, options.static);
-        }
-        for (var i = 0, len = this.routes.length; i < len; ++i) {
-            if (route.equals(this.routes[i])) {
-                return this.routes[i];
+        for (var i = 0, len = dynamics.length, matches; i < len; ++i) {
+            matches = request.path.match(dynamics[i].regexp);
+            if (matches) {
+                var args = matches.slice(1, matches.length);
+                args.splice(0, 0, request);
+                return {route: dynamics[i].route, args: args};
             }
         }
-    },
-
-    /**
-     * Match an URI and return a (target, urlargs) tuple
-     */
-    match: function(uri) {
-        if (uri in this.static) {
-            return [this.static[uri], []];
+        // late check to reduce overhead on hits
+        if (!compiled) {
+            compileRoutes();
+            return routeMatches(url);
         }
-        for (var i = 0, len = this.dynamic.length; i < len; ++i) {
-            match = uri.match(this.dynamic[i][0]);
-            if (!match) continue;
-            return [this.dynamic[i][1], match.slice(1, match.length)];
-        }
-        // Late check to reduce overhead on hits
-        if (!this.compiled) {
-            // Compile and try again.
-            this.compile();
-            return this.match(uri);
-        }
-        return [null, []];
-    },
-
-    /**
-     * Build an URI out of a named route and values for the wildcards.
-     */
-    build: function(name, options) {
-        if (name in this.named) {
-            return this.format(this.named[name], options);
-        } else {
-            // Late check to reduce overhead on hits
-            if (!this.compiled) {
-                // Compile and try again.
-                this.compile();
-                return this.build(name, options);
-            }
-        }
-    },
-
-    format: function(template, options) {
-        for (var key in options)
-            template = template.replace('#{' + key + '}', options[key]);
-        return template;
-    },
-
-    /**
-     * Build the search structures. Call this before actually using the router.
-     */
-    compile: function() {
-        this.reset();
-        for (var i = 0, len = this.routes.length, route = null; i < len; ++i) {
-            route = this.routes[i];
-            if (route.name) {
-                this.named[route.name] = route.format();
-            }
-            if (route.static()) {
-                this.static[route.route] = route.targets;
+    }
+    
+    // build the search structures. call this before actually using the router.
+    function compileRoutes() {
+        resetRoutes();
+        for (var i = 0, len = routes.length, route, regexp; i < len; ++i) {
+            route = routes[i];
+            if (isStatic(route)) {
+                statics[route.route] = route;
                 continue;
             }
-            var regexp = new RegExp('^' + route.group() + '$', 'i');
-            this.dynamic.push([regexp, route.targets]);
+            regexp = new RegExp('^' + group(route) + '$', 'i');
+            dynamics.push({regexp: regexp, route: route});
         }
-        this.dynamic.reverse();
-        this.compiled = true;
-    },
-
-    equals: function(router) {
-        return this.routes == router.routes;
+        dynamics.reverse();
+        compiled = true;
     }
-});
 
-/**
- * An observer reacts to changes of location hash
- */
-var Observer = function(router, stop) {
-    this.init(router, stop);
-};
-
-$.extend(Observer.prototype, {
-    interval: 100,
-
-    init: function(router, stop) {
-        this.router = router || new Router();
-        this.state = this.get();
-        this.callback(this.state);
-        if (!stop) this.start();
-    },
-
-    get: function() {
-        var hash = window.location.hash.replace(/^#/, '');
-        try {
-            return $.browser.mozilla ? hash : decodeURIComponent(hash);
-        }
-        catch (error) {
-            return hash;
-        }
-    },
-
-    set: function(hash) {
-        // IE will recognize strings following a question mark as real query string
-        window.location.hash = $.browser.msie && $.browser.version <= 6 ? hash.replace('?', '%3F') : hash;
-    },
-
-    start: function() {
-        this.stop();
-        this.timer = setInterval($.proxy(this.check, this), this.interval);
-    },
-
-    stop: function() {
-        if (this.timer) clearInterval(this.timer);
-    },
-
-    check: function() {
-        var current = this.get();
-        if (current != this.state) {
-            this.callback(current);
-            this.state = current;
-        }
-    },
-
-    callback: function(uri) {
-        var request = this.request(uri),
-            match = this.router.match(request.path);
-        if (match[0] && !$.isEmptyObject(match[0])) {
-            for (var i = 0, len = match[0].length; i < len; ++i) {
-                if ($.isFunction(match[0][i])) {
-                    match[0][i].apply(request, match[1]);
-                }
-            }
-        }
-    },
-
-    request: function(uri) {
-        var pieces = uri.split('?', 2);
-        return {
-            path: pieces[0],
-            params: $.unparam(pieces[1] || "")
-        };
-
-    },
-
-    reset: function() {
-        this.router = new Router;
-        if (this.timer) clearInterval(this.timer);
-        this.timer = null;
+    // clean route caches and set compiled flag to false
+    function resetRoutes() {
+        compiled = false;
+        statics = {};
+        dynamics = [];
     }
-});
 
-/**
- * Helpers
- */
-$.unparam = function (value) {
-    if (!value) return {};
-    // Object that holds names => values.
-    var params = {},
-        // Get query string pieces (separated by &)
-        pieces = value.split('&');
-
-    // Loop through query string pieces and assign params.
-    for (var i = 0, len = pieces.length, pair; i < len; ++i) {
-        pair = pieces[i].split('=', 2);
-        // Repeated parameters with the same name are overwritten. Parameters
-        // with no value get set to boolean true.
-        params[decodeURIComponent(pair[0])] = (pair.length == 2 ?
-            decodeURIComponent(pair[1].replace(/\+/g, ' ')) : true);
+    // convert url into a hash
+    function parseUrl(url) {
+        var pieces = url.split('?', 2);
+        return {path: pieces[0], params: unparam(pieces[1] || "")};
     }
-    return params;
-};
 
-/**
- * Public interfaces
- */
-$.router = new Router();
-$.router.observer = new Observer($.router, true);
+    function unparam(value) {
+        if (!value) return {};
+        // object that holds names => values.
+        var params = {},
+            // get query string pieces (separated by &)
+            pieces = value.split('&');
 
-$.router.route = function(route, options) {
-    return $.router.add(route, null, options);
-};
+        // loop through query string pieces and assign params.
+        for (var i = 0, len = pieces.length, pair; i < len; ++i) {
+            pair = pieces[i].split('=', 2);
+            // repeated parameters with the same name are overwritten. parameters
+            // with no value get set to boolean true.
+            params[decodeURIComponent(pair[0])] = (pair.length == 2 ?
+                decodeURIComponent(pair[1].replace(/\+/g, ' ')) : true);
+        }
+        return params;
+    }
 
-$.router.run = function() {
-    $.router.observer.start();
-};
-
-$.router.load = function(hash) {
-    $.router.observer.set(hash);
-};
-
-// })(jQuery);
+})(jQuery);
--- tests/router.js
+++ tests/router.js
@@ -3,150 +3,44 @@
 module("Route");
 
 test("Static", function() {
-    var route = new Route("/book/", "book");
-    equals(route.group().toString(), "\/book\/");
-    equals(route.format(), "/book/");
-    ok(route.static());
+    $.route('/book/', '#tmpl');
+    var result = $.routeMatches('/book/?test=false');
+
+    equals(result.route.route, '/book/');
+    equals(result.args[0].path, '/book/');
+    equals(result.args[0].params['test'], 'false');
 });
 
 test("Dynamic", function() {
-    var route = new Route("/book/:id/", "test");
-    equals(route.group().toString(), "\/book\/([^\/]+)\/");
-    equals(route.format(), "/book/#{id}/");
-    ok(!route.static());
+    $.route('/book/:id/', '#tmpl');
+    var result = $.routeMatches('/book/1/?test=true');
+
+    equals(result.route.route, '/book/:id/');
+    equals(result.args[0].path, '/book/1/');
+    equals(result.args[0].params['test'], 'true');
+    equals(result.args[1], '1');
 });
 
 test("Regex", function() {
-    var route = new Route("/book/:#\\d+#/", "test");
-    equals(route.group().toString(), "\/book\/(\\d+)\/");
-    equals(route.format(), "/book/#{anon0}/");
-    ok(!route.static());
+    $.route('/note/:#\\d+#/', '#tmpl');
+    var result = $.routeMatches('/note/2/?test=false&lang=en');
+
+    equals(result.route.route, '/note/:#\\d+#/');
+    equals(result.args[0].path, '/note/2/');
+    equals(result.args[0].params['test'], 'false');
+    equals(result.args[0].params['lang'], 'en');
+    equals(result.args[1], '2');
 });
 
 test("Dynamic with Regex", function() {
-    var route = new Route("/book/:id#\\d+#/", "test");
-    equals(route.group().toString(), "\/book\/(\\d+)\/");
-    equals(route.format(), "/book/#{id}/");
-    ok(!route.static());
-});
+    $.route('/comment/:id#\\d+#/', '#tmpl');
+    var result = $.routeMatches('/comment/3/?test&lang=en');
 
-module("Router");
-
-var router, observer, request,
-    add = function() {
-        router.add.apply(router, arguments);
-        router.compile();
-    },
-    match = function() {
-        return router.match.apply(router, arguments);
-    },
-    reset = function() {
-        router = new Router();
-        observer = new Observer(router);
-    },
-    build = function() {
-        return router.build.apply(router, arguments);
-    };
-
-test("Basic", function() {
-    reset();
-
-    add('/static/', 'static');
-    equals(match('/static/')[0], 'static');
-    ok($.isEmptyObject(match('/static/')[1]));
-
-    // Not supported yet since JavaScript does not support negative lookbehind assertion
-    // add('/\\:its/:#.+#/:test/:name#[a-z]+#/', 'handler');
-    // equals(match('/:its/a/cruel/world/'), ['handler', {'test': 'cruel', 'name': 'world'}]);
-
-    add('/:test', 'notail');
-    equals(match('/test')[0], 'notail');
-    equals(match('/test')[1][0], 'test');
-
-    add(':test/', 'nohead');
-    equals(match('test/')[0], 'nohead');
-    equals(match('test/')[1][0], 'test');
-
-    add(':test', 'fullmatch');
-    equals(match('test')[0], 'fullmatch');
-    equals(match('test')[1][0], 'test');
-
-    add('/:#anon#/match', 'anon');
-    equals(match('/anon/match')[0], 'anon');
-    equals(match('/anon/match')[1][0], 'anon');
-
-    ok(!match('//no/m/at/ch/')[0]);
-    ok($.isEmptyObject(match('//no/m/at/ch')[1]));
-});
-
-test("Parentheses", function() {
-    // Will match `notail` if router is not reset
-    reset();
-
-    add('/func(:param)', 'func');
-    equals(match('/func(foo)')[0], 'func')
-    equals(match('/func(foo)')[1][0], 'foo');
-
-    add('/func2(:param#foo|bar#)', 'func2');
-    equals(match('/func2(foo)')[0], 'func2');
-    equals(match('/func2(foo)')[1][0], 'foo');
-    equals(match('/func2(bar)')[0], 'func2');
-    equals(match('/func2(bar)')[1][0], 'bar');
-
-    ok(!match('/func2(baz)')[0]);
-    ok($.isEmptyObject(match('/func2(baz)')[1]));
-
-    add('/groups/:param#foo|bar#', 'groups');
-    equals(match('/groups/foo')[0], 'groups');
-    equals(match('/groups/foo')[1][0], 'foo');
-    ok(!match('/groups/foo')[1][1]);
-});
-
-test("Error in pattern", function() {
-});
-
-test("Build", function() {
-    reset();
-
-    add('/:test/:name#[a-z]+#/', 'handler', {name: 'testroute'})
-    add('/anon/:#.#', 'handler', {name: 'anonroute'})
-    equals(build('testroute', {test: 'hello', name: 'world'}), '/hello/world/');
-});
-
-module("Observer");
-
-test("Location", function() {
-    reset();
-    observer.set("/location/");
-    equals(window.location.hash, "#/location/");
-
-    equals(observer.get(), "/location/");
-});
-
-test("Request", function() {
-    reset();
-    request = observer.request("/test?redirect&v=1");
-    equals(request.path, "/test");
-    equals(request.params.redirect, true);
-    equals(request.params.v, "1");
-});
-
-test("Callback", function() {
-    var book = null;
-    reset();
-
-    stop(1000);
-    add("/book/:id/", function(id) {
-        book = id;
-        equals(book, id);
-
-        // Request
-        equals(this.path, "/book/3/");
-        equals(this.params.v, "2");
-        start();
-    });
-    observer.start();
-    observer.set("/book/3/?v=2");
+    equals(result.route.route, '/comment/:id#\\d+#/');
+    equals(result.args[0].path, '/comment/3/');
+    equals(result.args[0].params['test'], true);
+    equals(result.args[0].params['lang'], 'en');
+    equals(result.args[1], '3');
 });
 
 module("Helper")
